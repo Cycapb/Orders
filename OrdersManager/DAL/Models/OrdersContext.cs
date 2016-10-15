276ecba051d1f61c199b0296f1697023fc2c14a0
Apostol <Apostol@MAIN>
2016-10-15 16:52:08 +0500

    Added entities Order, OrderDetail, Product with EntityFrameWork Code First feature. Added type OrdersContext.

---
+++ OrdersManager/DAL/Models/OrdersContext.cs
@@ -0,0 +1,48 @@
+﻿using System.Data.Entity;
+
+namespace DAL.Models
+{
+    public class OrdersContext:DbContext
+    {
+        public OrdersContext():base("name: OrdersConnectionString")
+        {
+            Database.SetInitializer<OrdersContext>(null);
+        }
+
+        public virtual DbSet<Order> Order { get; set; }
+        public virtual DbSet<OrderDetail> OrderDetail { get; set; }
+        public virtual DbSet<Product> Product { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>()
+                .Property(e => e.Freight)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<Order>()
+                .Property(e => e.RowTimeStamp)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Order>()
+                .HasMany(e => e.OrderDetail)
+                .WithOptional(e => e.Order)
+                .WillCascadeOnDelete();
+
+            modelBuilder.Entity<OrderDetail>()
+                .Property(e => e.UnitPrice)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<OrderDetail>()
+                .Property(e => e.RowTimeStamp)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.UnitPrice)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.RowTimeStamp)
+                .IsFixedLength();
+        }
+    }
+}
