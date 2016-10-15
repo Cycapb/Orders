f5a5d0038fd52b4123a5bbcc6e416e7fd7ad3f8b
Apostol <Apostol@MAIN>
2016-10-15 16:58:24 +0500

    Added type IRepository<T>

---
+++ OrdersManager/DAL/Abstract/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DAL.Abstract
+{
+    public interface IRepository<T> where T:class
+    {
+        IEnumerable<T> GetItems();
+        Task<IEnumerable<T>> GetItemsAsync();
+        Task<T> GetItemAsync(int id);
+        Task DeleteAsync(int id);
+        Task UpdateAsync(int id);
+        Task SaveAsync();
+    }
+}
