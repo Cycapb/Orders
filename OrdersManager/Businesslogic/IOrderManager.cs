d8dc675494a653a9cbb67e24d0d1e05b49c312e0
Apostol <Apostol@MAIN>
2016-10-15 17:19:56 +0500

    Created type IOrderManager

---
+++ OrdersManager/Businesslogic/IOrderManager.cs
@@ -0,0 +1,12 @@
+﻿using DAL.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Businesslogic
+{
+    public interface IOrderManager
+    {
+        Task<IEnumerable<Order>> GetOrders();
+        Task UnloadToExcel();
+    }
+}
