71039c9747160a2b9a6e9df90ac11f11a2a36610
Apostol <Apostol@MAIN>
2016-10-16 22:12:38 +0500

    Created types IMailSettingsProvider and implemented it in MailSettingProvider. Added type EmailSettings. Added smtp settings into web.config

---
+++ OrdersManager/Businesslogic/EmailSettingsProvider.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using Domain;
+
+namespace Businesslogic
+{
+    public class EmailSettingsProvider:IMailSettingProvider
+    {
+        public EmailSettings GetSettings()
+        {
+            return new EmailSettings()
+            {
+                MailFrom = ConfigurationManager.AppSettings["MailFrom"],
+                UserName = ConfigurationManager.AppSettings["UserName"],
+                Password = ConfigurationManager.AppSettings["Password"],
+                ServerName = ConfigurationManager.AppSettings["Server"],
+                ServerPort = int.Parse(ConfigurationManager.AppSettings["Port"]),
+                UseSsl = bool.Parse(ConfigurationManager.AppSettings["UseSsl"])
+            };
+        }
+}
---
+++ OrdersManager/Businesslogic/IMailSettingProvider.cs
@@ -0,0 +1,9 @@
+﻿using Domain;
+
+namespace Businesslogic
+{
+    public interface IMailSettingProvider
+    {
+        EmailSettings GetSettings();
+    }
+}
