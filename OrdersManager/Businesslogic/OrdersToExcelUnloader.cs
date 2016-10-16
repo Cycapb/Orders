c1a6d09d537bb61da6e8d064b9d8d669a9e7cdef
Apostol <Apostol@MAIN>
2016-10-16 13:32:21 +0500

    Added type OrdersToExcelUnloader

---
+++ OrdersManager/Businesslogic/OrdersToExcelUnloader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Businesslogic
+{
+    public class OrdersToExcelUnloader:IUnloader<OrdersToExcelUnloader>
+    {
+        public void Unload(IEnumerable<OrdersToExcelUnloader> items)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
