ccd0f9ebf3f48e4845b99fa88147ef8e65008998
Apostol <Apostol@MAIN>
2016-10-17 09:32:25 +0500

    Created type BundleConfig and registered it in global.asax. Added bundles to _Layout.

---
+++ OrdersManager/WebUI/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Optimization;
+
+namespace WebUI
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery-ajax").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery.unobtrusive-ajax.js",
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/bootstrap-datepicker.js",
+                "~/Scripts/Scripts.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/bootstrap-theme.css",
+                "~/Content/bootstrap-datepicker3.css",
+                "~/Content/Site.css"));
+        }
+    }
+}
